55f63dfd8c4ff75f7136e495fa65f4f0660a7ab6
ariel.feil <Afeil@tmoviles.com.ar>
2025-02-26 16:39:44 -0300

    add targert

---
+++ minigreo/target/debug/deps/minigreo-c5f92c821c67ccd4.d
@@ -0,0 +1,5 @@
+C:\Cloud\Cursos\Rust\UdemyChilenoAppGrep\minigreo\target\debug\deps\libminigreo-c5f92c821c67ccd4.rmeta: src\main.rs
+
+C:\Cloud\Cursos\Rust\UdemyChilenoAppGrep\minigreo\target\debug\deps\minigreo-c5f92c821c67ccd4.d: src\main.rs
+
+src\main.rs:
